225bda92288e26265c2219cbee4dd3252bbd46eb
Nethsara Liyanage <tharusharb1012@gmail.com>
2022-03-06 18:25:50 +0530

    Sec pwd add

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/secureSplash',
+      initialRoute: '/secPwdSplash',
       routes: {
         '/secureSplash': (context) => const secure_notes_splash(),
         '/secureNotesHome': (context) => secure_notes_home(),
