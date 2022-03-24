4ab582863743d2f2f25f8f478e50868ce276d931
M.L.Akila Liyanage <IT19120812@my.sliit.lk>
2022-03-24 16:45:12 +0530

    minor chnages

--- save_my_pin_be/routes/secure_notes.js
+++ save_my_pin_be/routes/secure_notes.js
@@ -1,11 +1,32 @@
-const express = require('express')
+const express = require('express');
+const req = require('express/lib/request');
 const app = express()
 
 const router = express.Router({});
+
+//models
+const SecureNote = require('../models/secure_note_model')
 
 
 router.get('/get-all', async (_req, res, _next) => {
 	
 });
+
+router.post('/create-new', async (_req, res, _next) => {
+	try{
+
+        var newSecureNote = new SecureNote({
+            noteName : _req.body.noteName ?? "",
+            body : _req.body.body ?? "",
+            hasOptCode : _req.body.hasOptCode ?? false,
+            optPassCode : _req.body.hasOptCode ? _req.body.optPassCode : null,
+        })
+
+        newSecureNote
+    }catch(e){
+
+    }
+});
+
 // export router with all routes included
 module.exports = router;
