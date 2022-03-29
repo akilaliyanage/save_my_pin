3133c547b3bb503826a231c070a470a44e3825fc
M.L.Akila Liyanage <IT19120812@my.sliit.lk>
2022-03-29 16:34:10 +0530

    secure note delete done

--- lib/models/SecureNotesModel.dart
+++ lib/models/SecureNotesModel.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/foundation.dart';
 
 class SecureNote {
+  late final String id;
   late final String noteName;
   late final String body;
   late final bool hasOptCode;
   late final String optPassCode;
 
   SecureNote({
+    required this.id,
     required this.noteName,
     required this.body,
     required this.hasOptCode,
@@ -15,6 +17,7 @@ class SecureNote {
 
   factory SecureNote.fromJson(Map<String, dynamic> json) {
     return SecureNote(
+        id: json['_id'],
         noteName: json['noteName'],
         body: json['body'],
         hasOptCode: json['hasOptCode'],
--- lib/pages/akila/list_container.dart
+++ lib/pages/akila/list_container.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:save_my_pin/api/http_service_secure_notes.dart';
 
 import 'package:save_my_pin/models/SecureNotesModel.dart';
@@ -33,19 +34,16 @@ class SecureNotesList extends StatelessWidget {
 
           return Padding(
               padding: const EdgeInsets.only(
-                // left: kDefaultPadding,
-                // right: kDefaultPadding,
-                //bottom: kDefaultPadding * 0.5,
-              ),
+                  // left: kDefaultPadding,
+                  // right: kDefaultPadding,
+                  //bottom: kDefaultPadding * 0.5,
+                  ),
               child: Column(
-                children: dataList.map(
+                children: dataList
+                    .map(
                       (SecureNote e) => Column(
                         children: <Widget>[
-                          secureNoteCard(
-                            context,
-                            e.noteName,
-                            e.body,
-                          ),
+                          secureNoteCard(context, e.noteName, e.body, e.id),
                         ],
                       ),
                     )
@@ -55,12 +53,65 @@ class SecureNotesList extends StatelessWidget {
   }
 }
 
-Widget secureNoteCard(BuildContext context, String noteName, String body) {
+Widget secureNoteCard(
+    BuildContext context, String noteName, String body, String id) {
   Size? size = MediaQuery?.of(context).size;
 
   return Dismissible(
-    onDismissed: (DismissDirection direction) {},
-    key: UniqueKey(),
+    onDismissed: (DismissDirection direction) {
+      //delete the note
+      Alert(
+        context: context,
+        type: AlertType.warning,
+        title: "DELETE NOTE?",
+        desc: "This action is irreveesable. Want to proceed?",
+        buttons: [
+          DialogButton(
+            child: Text(
+              "DELETE",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () async {
+              HttpSecureNote secureNote = new HttpSecureNote();
+
+              bool isDel = await secureNote.delNote(id);
+
+              if(isDel){
+                Alert(
+                    context: context,
+                    type: AlertType.success,
+                    title: "DELETE SUCCESSSFUL!!",
+                    //desc: "Flutter is more awesome with RFlutter Alert.",
+                    buttons: [
+                      DialogButton(
+                        child: Text(
+                          "OK",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        onPressed: () => Navigator.pop(context),
+                        color: Color.fromRGBO(0, 179, 134, 1.0),
+                      )
+                    ],
+                  ).show();
+              }
+            },
+            color: Color.fromRGBO(0, 179, 134, 1.0),
+          ),
+          DialogButton(
+            child: Text(
+              "CANCEL",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () => Navigator.pop(context),
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 255, 0, 0),
+              Color.fromARGB(255, 255, 8, 0)
+            ]),
+          )
+        ],
+      ).show();
+    },
+    key: Key(id),
     secondaryBackground: Container(
       child: Center(
         child: Text(
