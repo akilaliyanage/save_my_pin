9732ff7736934918c29fd60270f145a666a0177b
MahendraThammita <dhmmpthammita@gmail.com>
2022-04-09 02:58:21 +0530

    Bug fix in update pin

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
-      initialRoute: '/secPwdSplash',
+      initialRoute: Login.routeName,
 
 
       routes: {
